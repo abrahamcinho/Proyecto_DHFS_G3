d3b1df2bebfe93b20e49aef67e83aff5a07e4653
CrisMarin58 <cgabrielamarin@gmail.com>
2022-02-14 13:21:06 -0300

    cambios al home para cargar aleatoriamente algunos productos

--- web/controllers/home_Ctrl.js
+++ web/controllers/home_Ctrl.js
@@ -3,7 +3,15 @@ const db = require('../config/dataBase_config');
 const home_Ctrl = {
 	listAll: (req, res) => {
 		db.ProductsCateg.findAll()
-		.then((categories) => res.render('home', { categories: categories}))
+		.then((categories) => {
+			db.Products.findAll()
+			.then((products) => {
+				const prods = products.sort(() => Math.random() - 0.5);
+				const prod = products.sort(() => Math.random() * 9);
+				res.render('home', { categories: categories, products: products, prods: prods, prod: prod })
+			})
+			.catch((e) => console.log(e));	
+		})
 		.catch((e) => console.log(e));
 	}
 };
