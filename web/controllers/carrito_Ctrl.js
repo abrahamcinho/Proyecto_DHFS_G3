385dc92944ddd912930b63576e4ce649b11bfe6d
CrisMarin58 <cgabrielamarin@gmail.com>
2022-02-22 15:45:55 -0300

    CRUD carrito de compras, aun no esta integrado con la vista

--- web/controllers/carrito_Ctrl.js
+++ web/controllers/carrito_Ctrl.js
@@ -5,6 +5,38 @@ const carrito_Ctrl = {
         db.Chart.findAll({ where: { user_id: req.session.userLogged.user_id } })
         .then((carrito) => res.render('carritoDeCompras', { carrito: carrito, user: req.session.userLogged }))
         .catch((e) => console.log(e));
+    },
+    addToCart: (req, res) => {
+        db.Chart.create({
+            user_id: req.body.user_id,
+            prod_id: req.body.prod_id,
+            flavor_id: req.body.flavor_id,
+            size_id: req.body.size_id,
+            quantity: req.body.quantity
+        })
+        .then(() => res.redirect('/carrito'))
+        .catch((e) => console.log(e));
+    },
+    updateCart: (req, res) => {
+        db.Chart.update({
+            user_id: req.body.user_id,
+            prod_id: req.body.prod_id,
+            flavor_id: req.body.flavor_id,
+            size_id: req.body.size_id,
+            quantity: req.body.quantity
+        })
+        .then(() => res.redirect('/carrito'))
+        .catch((e) => console.log(e));
+    },
+    deleteOne: (req, res) => {
+        db.Chart.destroy({ where: { user_id: req.params.id }, include: [{ prod_id: req.params.prod_id }] })
+        .then(() => res.redirect('/carrito'))
+        .catch((e) => console.log(e));
+    },
+    clearCart: (req, res) => {
+        db.Chart.destroy()
+        .then(() => res.redirect('/carrito'))
+        .catch((e) => console.log(e));
     }
 }
 
